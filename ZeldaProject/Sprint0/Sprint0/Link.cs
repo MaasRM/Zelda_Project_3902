47eeedc75134177e0c56e6c11af73bebf4e82e48
Jacob Kline <jrksoccer823@yahoo.com>
2021-02-07 15:32:58 -0500

    added Link class

---
+++ ZeldaProject/Sprint0/Sprint0/Link.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0
+{
+    class Link : IPlayer
+    {
+        // Instance of LinkStateMachine
+        public void Update()
+        {
+            //Call LinkStateMachineMethods
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //Call LinkStateMachineMethods
+        }
+    }
+}
