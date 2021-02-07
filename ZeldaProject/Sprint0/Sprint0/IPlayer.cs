d1166f0df36bc1ca0954cf47ea7637977d5bec14
Jacob Kline <jrksoccer823@yahoo.com>
2021-02-07 14:33:16 -0500

    Added IPlayer class

---
+++ ZeldaProject/Sprint0/Sprint0/IPlayer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0
+{
+    public interface IPlayer
+    {
+        public void Update();
+        public void Draw(SpriteBatch spriteBatch);
+    }
+}
