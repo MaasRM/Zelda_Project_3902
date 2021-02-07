9aa4f0ca1b64305440cfbbc571a7b765c552a0d4
Riley <Riley@DESKTOP-9TP96O7>
2021-02-07 17:00:46 -0500

    LinkStateMachineUpdated

--- ZeldaProject/Sprint0/Sprint0/LinkStateMachine.cs
+++ ZeldaProject/Sprint0/Sprint0/LinkStateMachine.cs
@@ -4,45 +4,50 @@ using System.Text;
 
 namespace Sprint0
 {
+    public enum Direction
+    {
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight
+    }
+
+    public enum Color
+    {
+        Green,
+        Red,
+        White
+    }
+
+    public enum Animation
+    {
+        Idle,
+        Walk,
+        Attack
+    }
+
     public class LinkStateMachine
     {
-        private enum Direction;
-        private enum Color;
-        private enum Animation;
+        private Direction direction;
+        private Color color;
+        private Animation animation;
         private Boolean useItem
         private Boolean isDamaged;
 
         // Enums for direction, color, etc
         public LinkStateMachine()
         {
-            enum Direction
-            {
-                MoveUp,
-                MoveDown,
-                MoveLeft,
-                MoveRight
-            }
-
-            enum Color
-            {
-                Green,
-                Red,
-                White
-            }
-
-            enum Animation
-            {
-                Idle,
-                Walk,
-                Attack
-            }
-
+            direction = MoveRight;
+            color = Green;
+            animation = Idle;
             useItem = false;
-
             isDamaged = false;
         }
         
-        
-        // get/set methods for the fields
+        public Rectangle getSprite(){
+            //LinkSpriteFactory, sends current state, returns the rectangle found
+            //LinkSpriteFactory.getRectangle(this.Direction, this.Color, );
+        }
+
     }
 }
