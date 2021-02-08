3b14a5cb066df77c88fc1e653b63490103253b7e
Riley <Riley@DESKTOP-9TP96O7>
2021-02-08 16:01:08 -0500

    LinkSpriteFactory Added

---
+++ ZeldaProject/Sprint0/Sprint0/LinkSpriteFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class LinkSpriteFactory
+{
+    public enum Direction
+    {
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight
+    }
+
+    public enum Color
+    {
+        Green,
+        Red,
+        White
+    }
+
+    public enum Animation
+    {
+        Idle,
+        Walk,
+        Attack
+    }
+
+    public LinkSpriteFactory()
+    {
+
+    }
+
+    public Rectangle getRectangle(Direction direction, Color color, Animation animation, Boolean useItem, Boolean isDamaged)
+    {
+        //call methods to check state
+    }
+
+}
