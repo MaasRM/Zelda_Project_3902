9fb960301831e570ab0949a036c5528f4626d5f6
Riley <Riley@DESKTOP-9TP96O7>
2021-02-13 19:53:42 -0500

    RectangleLinkMoveUpIdle

---
+++ ZeldaProject/Sprint0/Sprint0/RectangleLinkMoveUpIdle.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0
+{
+
+	public class RectangleLinkMoveUpIdle
+	{
+		public RectangleLinkMoveUpIdle()
+		{
+		}
+
+		public Rectangle getRectangle(LinkColor color)
+		{
+			Rectangle retRectangle;
+			if (color == LinkColor.Green)
+            {
+				retRectangle = new Rectangle(69, 11, 15, 15);
+			} else if (color == LinkColor.Red)
+            {
+				retRectangle = new Rectangle(69, 11, 15, 15);
+			} else //LinkColor.White
+            {
+				retRectangle = new Rectangle(69, 11, 15, 15);
+			}
+			return retRectangle;
+		}
+
+	}
+
+}
