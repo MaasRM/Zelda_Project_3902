2a5b05fdce2f8dd952b08d8c95b071a185f442af
nathanschultz <schultz.722@buckeyemail.osu.edu>
2021-02-17 02:42:54 -0500

    Goriya Boomerang initial push

---
+++ ZeldaProject/Sprint0/Sprint0/GoriyaBoomerang.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Sprint0
+{
+    public class GoriyaBoomerang : IProjectile
+    {
+        public GoriyaBoomerang()
+        {
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
