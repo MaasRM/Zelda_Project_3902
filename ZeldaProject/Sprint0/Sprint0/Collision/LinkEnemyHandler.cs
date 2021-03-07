d6ad583ea09eb6a986993050d3ca38b538ee8f24
Phil Brocker <brocker.15@osu.edu>
2021-03-07 14:37:07 -0500

    Creation of CollisionHandler folder.

---
+++ ZeldaProject/Sprint0/Sprint0/Collision/LinkEnemyHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace Sprint0
+{
+    public class LinkEnemyHandler
+    {
+
+
+        public LinkEnemyHandler()
+        {
+            
+        }
+    }
+}
