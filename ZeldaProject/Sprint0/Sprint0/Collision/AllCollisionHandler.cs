6a2d82a3c46f79d312c78eca432f0916d1bc43b3
nathanschultz <schultz.722@buckeyemail.osu.edu>
2021-03-07 15:13:09 -0500

    Added reference to gameObjects

--- ZeldaProject/Sprint0/Sprint0/Collision/AllCollisionHandler.cs
+++ ZeldaProject/Sprint0/Sprint0/Collision/AllCollisionHandler.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 namespace Sprint0
 {
     public class AllCollisionHandler
     {
+        private Tuple<IPlayer, List<IBlock>, List<IItem>, List<INPC>, List<IProjectile>> gameObjects;
 
-        public AllCollisionHandler()
+        public AllCollisionHandler(Tuple<IPlayer, List<IBlock>, List<IItem>, List<INPC>, List<IProjectile>> obs)
         {
-            
+            gameObjects = obs;
         }
     }
 }
