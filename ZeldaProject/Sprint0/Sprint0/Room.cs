feb229638757d93065db24e9bf6e40a3f302f386
Riley <Riley@DESKTOP-9TP96O7>
2021-03-10 14:04:25 -0500

    Barebones Room.cs class added, subject to change

---
+++ ZeldaProject/Sprint0/Sprint0/Room.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0
+{
+    public class Room
+    {
+        private List<IBlock> blocks;
+        private List<IItem> items;
+        private List<INPC> npcs;
+
+        public Room(List<IBlock> blocks, List<IItem> items, List<INPC> npcs)
+        {
+            this.blocks = blocks;
+            this.items = items;
+            this.npcs = npcs;
+        }
+
+    }
+}
