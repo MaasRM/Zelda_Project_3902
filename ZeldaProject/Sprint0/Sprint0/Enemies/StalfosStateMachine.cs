bb1aaf4d2ad6904785700b88fd505da7840bf868
nathanschultz <schultz.722@buckeyemail.osu.edu>
2021-03-14 14:02:39 -0400

    Implemented collision

--- ZeldaProject/Sprint0/Sprint0/Enemies/StalfosStateMachine.cs
+++ ZeldaProject/Sprint0/Sprint0/Enemies/StalfosStateMachine.cs
@@ -16,12 +16,25 @@ namespace Sprint0
             Right
         }
 
+        private enum State
+        {
+            Normal,
+            Damaged,
+            Stun
+        }
+
         private Direction direction;
+        private State state;
+        private Vector2 damageDirection;
         private int xLoc;
         private int yLoc;
         private int width;
         private int height;
         private int frame;
+        private int stunFrames;
+        private int damageFrames;
+        private int health;
+        private int MAXHEALTH = 4;
         private const int PIXELSCALER = 4;
         private const int moveDist = 2;
 
@@ -32,6 +45,11 @@ namespace Sprint0
             width = 16;
             height = 16;
             frame = -1;
+            stunFrames = 0;
+            damageFrames = 0;
+            health = MAXHEALTH;
+            state = State.Normal;
+            damageDirection = new Vector2(0, 0);
         }
 
         public Rectangle GetDestination()
@@ -52,30 +70,43 @@ namespace Sprint0
 
         public void Move()
         {
-            frame++;
+            if(state == State.Normal || (state == State.Damaged && damageDirection == new Vector2(0,0)))
+            {
+                frame++;
 
-            if (frame % 8 == 0)
-            {
-                direction = ChangeDirection();
-            }
+                if (frame % 8 == 0)
+                {
+                    direction = ChangeDirection();
+                }
 
-            if(direction == Direction.Up)
-            {
-                yLoc -= moveDist * PIXELSCALER;
-            }
+                if (direction == Direction.Up)
+                {
+                    yLoc -= moveDist * PIXELSCALER;
+                }
 
-            else if (direction == Direction.Down)
-            {
-                yLoc += moveDist * PIXELSCALER;
-            }
+                else if (direction == Direction.Down)
+                {
+                    yLoc += moveDist * PIXELSCALER;
+                }
 
-            else if (direction == Direction.Left)
-            {
-                xLoc -= moveDist * PIXELSCALER;
+                else if (direction == Direction.Left)
+                {
+                    xLoc -= moveDist * PIXELSCALER;
+                }
+                else
+                {
+                    xLoc += moveDist * PIXELSCALER;
+                }
             }
-            else
+            else if(state == State.Damaged)
             {
-                xLoc += moveDist * PIXELSCALER;
+                xLoc += (int)damageDirection.X;
+                yLoc += (int)damageDirection.Y;
+                damageFrames++;
+            }
+            else if(state == State.Stun)
+            {
+                stunFrames++;
             }
         }
 
@@ -89,6 +120,41 @@ namespace Sprint0
             int num = RandomNumberGenerator.GetInt32(4);
 
             return (Direction) num;
+        }
+
+        public bool HasHealth()
+        {
+            return health > 0;
+        }
+
+        public void TakeDamage(int damage, Vector2 direction)
+        {
+            if (state != State.Damaged)
+            {
+                health -= damage;
+                state = State.Damaged;
+                stunFrames = 0;
+                damageFrames = 0;
+
+                damageDirection = direction;
+            }
+        }
+
+        public void SetStun()
+        {
+            state = State.Stun;
+            stunFrames = 0;
+        }
+
+        public void ReturnToNormal()
+        {
+            if (damageFrames > 24 || stunFrames > 60)
+            {
+                state = State.Normal;
+                stunFrames = 0;
+                damageFrames = 0;
+                damageDirection = new Vector2(0, 0);
+            }
         }
     }
 }
