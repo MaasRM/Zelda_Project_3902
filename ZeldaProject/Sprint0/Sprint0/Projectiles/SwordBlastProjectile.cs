b9279d3977b7dd3f7cf313df528bff0ade558545
Jacob Kline <jrksoccer823@yahoo.com>
2021-03-28 17:42:47 -0400

    Implimented sword blast

---
+++ ZeldaProject/Sprint0/Sprint0/Projectiles/SwordBlastProjectile.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System.Text;
+
+namespace Sprint0
+{
+    class SwordBlastProjectile : IProjectile, IPlayerProjectile
+    {
+        //This class is for the sword beam mechanic when link is at full health. Not used in sprint 2
+
+        private Rectangle sourceRectangle;
+        private Rectangle destinationRectangle;
+        private Texture2D spritesheet;
+        private int xLoc;
+        private int yLoc;
+        private const int blastSpeed = 10; //x4 specs
+        private const int DAMAGE = 0;
+        private int frame;
+        private const int HITFRAME = 5;
+        private Direction projectileVerticleDirection;
+        private Direction projectileHorizontalDirection;
+        private SpriteEffects flip;
+        public SwordBlastProjectile(Texture2D spritesheet, int x, int y, Direction verticleDirection, Direction horizontalDirection, SpriteEffects flip)
+        {
+            this.spritesheet = spritesheet;
+            xLoc = x;
+            yLoc = y;
+            projectileVerticleDirection = verticleDirection;
+            projectileHorizontalDirection = horizontalDirection;
+            this.flip = flip;
+            destinationRectangle = new Rectangle(xLoc, yLoc, 30, 60);
+            sourceRectangle = new Rectangle(27, 155, 8, 15);
+            frame = 0;
+        }
+
+        public void Update()
+        {
+            frame++;
+            sourceRectangle.Offset(35 * (int)Math.Pow(-1, (frame % 2) + 1), 0);
+            if(projectileVerticleDirection == Direction.MoveUp)
+            {
+                yLoc -= blastSpeed;
+            } else
+            {
+                yLoc += blastSpeed;
+            }
+            if (projectileHorizontalDirection == Direction.MoveLeft)
+            {
+                xLoc -= blastSpeed;
+            }
+            else
+            {
+                xLoc += blastSpeed;
+            }
+            destinationRectangle = new Rectangle(xLoc, yLoc, 30, 60);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(spritesheet, destinationRectangle, sourceRectangle, Color.White, 0, new Vector2(0, 0), flip, 0f);
+        }
+
+        public Rectangle GetProjectileLocation()
+        {
+            return destinationRectangle;
+        }
+
+        public bool CheckForRemoval()
+        {
+            return frame >= HITFRAME;
+        }
+
+        public int GetDamage()
+        {
+            return DAMAGE;
+        }
+
+        public void Hit()
+        {
+            //This projectile doesn't interact
+        }
+    }
+}
--- ZeldaProject/Sprint0/Sprint0/Projectiles/SwordProjectile.cs
+++ ZeldaProject/Sprint0/Sprint0/Projectiles/SwordProjectile.cs
@@ -10,46 +10,129 @@ namespace Sprint0
     {
         //This class is for the sword beam mechanic when link is at full health. Not used in sprint 2
 
-        private LinkStateMachine stateMachine;
+        private Rectangle sourceRectangle;
+        private Rectangle destinationRectangle;
         private Texture2D spritesheet;
-        private int damage;
-        private bool remove;
-        public SwordProjectile(Texture2D spritesheet, LinkStateMachine stateMachine)
+        private Sprint3 game;
+        private int xLoc;
+        private int yLoc;
+        private int xSize;
+        private int ySize;
+        private const int SwordSpeed = 30; //x4 specs
+        private const int DAMAGE = 2;
+        private int SwordLength = 60;
+        private int SwordWidth = 30;
+        private int frame;
+        private const int HITFRAME = 20;
+        private Direction projectileDirection;
+        private SpriteEffects flip;
+        public SwordProjectile(Texture2D spritesheet, LinkStateMachine stateMachine, Sprint3 game)
         {
-            this.stateMachine = stateMachine;
             this.spritesheet = spritesheet;
-            remove = false;
-            damage = 1;
+            this.game = game;
+            projectileDirection = stateMachine.getDirection();
+            if (projectileDirection == Direction.MoveUp)
+            {
+                xSize = SwordWidth;
+                ySize = SwordLength;
+                xLoc = stateMachine.getXLoc() + xSize / 2;
+                yLoc = stateMachine.getYLoc() - ySize;
+                flip = SpriteEffects.None;
+            }
+            else if (projectileDirection == Direction.MoveDown)
+            {
+                xSize = SwordWidth;
+                ySize = SwordLength;
+                xLoc = stateMachine.getXLoc() + xSize / 2;
+                yLoc = stateMachine.getYLoc() + ySize;
+                flip = SpriteEffects.FlipVertically;
+            }
+            else if (projectileDirection == Direction.MoveLeft)
+            {
+                xSize = SwordLength;
+                ySize = SwordLength;
+                xLoc = stateMachine.getXLoc() - xSize;
+                yLoc = stateMachine.getYLoc() + ySize / 8;
+                flip = SpriteEffects.FlipHorizontally;
+            }
+            else //MoveRight
+            {
+                xSize = SwordLength;
+                ySize = SwordLength;
+                xLoc = stateMachine.getXLoc() + xSize;
+                yLoc = stateMachine.getYLoc() + ySize / 4;
+                flip = SpriteEffects.None;
+            }
+            if (projectileDirection == Direction.MoveUp || projectileDirection == Direction.MoveDown)
+            {
+                sourceRectangle = new Rectangle(1, 154, 8, 15);
+            }
+            else
+            {
+                sourceRectangle = new Rectangle(10, 154, 15, 15);
+            }
+            destinationRectangle = new Rectangle(xLoc, yLoc, xSize, ySize);
+            frame = 0;
         }
 
         public void Update()
         {
-
+            frame++;
+            sourceRectangle.Offset(35 * (int)Math.Pow(-1, (frame % 2) + 1), 0);
+            if (frame < HITFRAME)
+            {
+                if (projectileDirection == Direction.MoveUp)
+                {
+                    yLoc -= SwordSpeed;
+                }
+                else if (projectileDirection == Direction.MoveDown)
+                {
+                    yLoc += SwordSpeed;
+                }
+                else if (projectileDirection == Direction.MoveLeft)
+                {
+                    xLoc -= SwordSpeed;
+                }
+                else //MoveRight
+                {
+                    xLoc += SwordSpeed;
+                }
+                destinationRectangle = new Rectangle(xLoc, yLoc, xSize, ySize);
+            } else
+            {
+                game.AddProjectile(new SwordBlastProjectile(spritesheet, xLoc - 30, yLoc - 10, Direction.MoveUp, Direction.MoveLeft, SpriteEffects.None));
+                game.AddProjectile(new SwordBlastProjectile(spritesheet, xLoc, yLoc - 10, Direction.MoveUp, Direction.MoveRight, SpriteEffects.FlipHorizontally));
+                game.AddProjectile(new SwordBlastProjectile(spritesheet, xLoc - 30, yLoc + 10, Direction.MoveDown, Direction.MoveLeft, SpriteEffects.FlipVertically));
+                game.AddProjectile(new SwordBlastProjectile(spritesheet, xLoc, yLoc + 10, Direction.MoveDown, Direction.MoveRight, SpriteEffects.FlipVertically | SpriteEffects.FlipHorizontally));
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Draw(spritesheet, destinationRectangle, sourceRectangle, Color.White, 0, new Vector2(0, 0), flip, 0f);
         }
 
         public Rectangle GetProjectileLocation()
         {
-            return new Rectangle(1, 1, 1, 1);
+            return destinationRectangle;
         }
 
         public bool CheckForRemoval()
         {
-            return remove;
+            return frame >= HITFRAME;
         }
 
         public int GetDamage()
         {
-            return damage;
+            return DAMAGE;
         }
 
         public void Hit()
         {
-            remove = true;
+            if (frame < HITFRAME)
+            {
+                frame = HITFRAME;
+            }
         }
     }
 }
