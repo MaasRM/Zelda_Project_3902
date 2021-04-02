b3a16b1956dc13e1d47271eadbade3be0ab9adcd
Riley <Riley@DESKTOP-9TP96O7>
2021-04-02 12:16:24 -0400

    Added Phil's door changes back into the project

--- ZeldaProject/Sprint0/Sprint0/Collision/LinkWallHandler.cs
+++ ZeldaProject/Sprint0/Sprint0/Collision/LinkWallHandler.cs
@@ -24,6 +24,12 @@ namespace Sprint0
             if (player.getLinkStateMachine().getYLoc() > (288 + (64 * 4)) && player.getLinkStateMachine().getYLoc() + player.LinkPosition().Height < (416 + (64 * 4)))
             {
                 isSwapped = room.SwapRoom(Direction.MoveLeft);
+                if (!isSwapped && player.GetLinkInventory().getKeyCount() > 0)
+                {
+                    room.UnlockDoor(Direction.MoveLeft);
+                    isSwapped = room.SwapRoom(Direction.MoveLeft);
+                    room.UnlockDoor(Direction.MoveRight);
+                }
                 if (isSwapped)
                 {
                     Rectangle newPosition = new Rectangle(maxX - 175, player.getLinkStateMachine().getYLoc(), player.LinkPosition().Width, player.LinkPosition().Height);
@@ -48,6 +54,12 @@ namespace Sprint0
             if (player.getLinkStateMachine().getXLoc() > 448 && player.getLinkStateMachine().getXLoc() + player.LinkPosition().Width < 576)
             {
                 isSwapped = room.SwapRoom(Direction.MoveUp);
+                if (!isSwapped && player.GetLinkInventory().getKeyCount() > 0)
+                {
+                    room.UnlockDoor(Direction.MoveUp);
+                    isSwapped = room.SwapRoom(Direction.MoveUp);
+                    room.UnlockDoor(Direction.MoveDown);
+                }
                 if (isSwapped)
                 {
                     Rectangle newPosition = new Rectangle(player.getLinkStateMachine().getXLoc(), maxY - 185, player.LinkPosition().Width, player.LinkPosition().Height);
@@ -72,6 +84,12 @@ namespace Sprint0
             if (player.getLinkStateMachine().getYLoc() > (288 + (64 * 4)) && player.getLinkStateMachine().getYLoc() + player.LinkPosition().Height < (416 + (64 * 4)))
             {
                 isSwapped = room.SwapRoom(Direction.MoveRight);
+                if (!isSwapped && player.GetLinkInventory().getKeyCount() > 0)
+                {
+                    room.UnlockDoor(Direction.MoveRight);
+                    isSwapped = room.SwapRoom(Direction.MoveRight);
+                    room.UnlockDoor(Direction.MoveLeft);
+                }
                 if (isSwapped)
                 {
                     Rectangle newPosition = new Rectangle(120, player.getLinkStateMachine().getYLoc(), player.LinkPosition().Width, player.LinkPosition().Height);
@@ -96,6 +114,12 @@ namespace Sprint0
             if (player.getLinkStateMachine().getXLoc() > 448 && player.getLinkStateMachine().getXLoc() + player.LinkPosition().Width < 576)
             {
                 isSwapped = room.SwapRoom(Direction.MoveDown);
+                if (!isSwapped && player.GetLinkInventory().getKeyCount() > 0)
+                {
+                    room.UnlockDoor(Direction.MoveDown);
+                    isSwapped = room.SwapRoom(Direction.MoveDown);
+                    room.UnlockDoor(Direction.MoveUp);
+                }
                 if (isSwapped)
                 {
                     Rectangle newPosition = new Rectangle(player.getLinkStateMachine().getXLoc(), 117 + (64 * 4), player.LinkPosition().Width, player.LinkPosition().Height);
