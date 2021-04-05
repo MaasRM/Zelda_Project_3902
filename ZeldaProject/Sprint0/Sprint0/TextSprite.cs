6af119cbb2eb92086f1fc482370915f3cdcb536f
Jacob Kline <jrksoccer823@yahoo.com>
2021-04-04 23:26:32 -0400

    More changes to TextSprite

--- ZeldaProject/Sprint0/Sprint0/TextSprite.cs
+++ ZeldaProject/Sprint0/Sprint0/TextSprite.cs
@@ -25,9 +25,9 @@ namespace Sprint0
               65, 27, 113, 11, 57, 11, 46, 11, 105, 19, 57, 11, 113, 19, 49, 27 // secret. (16)
             };
         readonly private int[] letterDest =
-            { 0, 0, 61, 48, 68, 48, 75, 48, 82, 48, 89, 48, 96, 48, 103, 48, 110, 48, 117, 48, 124, 48, 131, 48, 138, 48, 145, 48, 152, 48, 159, 48, 166, 48, 173, 48, 180, 48, 187, 48, //top 19 letters (40)
+            { 54, 48, 61, 48, 68, 48, 75, 48, 82, 48, 89, 48, 96, 48, 103, 48, 110, 48, 117, 48, 124, 48, 131, 48, 138, 48, 145, 48, 152, 48, 159, 48, 166, 48, 173, 48, 180, 48, 187, 48, //top 19 letters (40)
               75, 55, 82, 55, 89, 55, 96, 55, 103, 55, 110, 55, 117, 55, 124, 55, 131, 55, 138, 55, 145, 55, 152, 55, 159, 55, 166, 55 //bottom 14 letters (28)
-            }; // 54, 48
+            };
 
         public TextSprite(Texture2D dungeonSheet, Sprint3 game)
         {
@@ -44,10 +44,10 @@ namespace Sprint0
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            for (int i = 0; i < counter * 2; i += 2)
+            for (int i = 0; i <= counter * 2; i += 2)
             {
-                Rectangle destination = new Rectangle(letterDest[i], letterDest[i + 1], 7 * scale, 7 * scale);
-                Rectangle source = new Rectangle((letterSource[i] + 37) * scale, (64 * scale) + (letterSource[i + 1] + 37) * scale, 7 * scale, 7 * scale);
+                Rectangle destination = new Rectangle((letterDest[i] + 37) * scale, (64 * scale) + (letterDest[i + 1] + 37) * scale, 7 * scale, 7 * scale);
+                Rectangle source = new Rectangle(letterSource[i], letterSource[i + 1], 7 * scale, 7 * scale);
                 spriteBatch.Draw(letterSheet, destination, source, Color.White);
             }
             //textSounds[0].Play();
