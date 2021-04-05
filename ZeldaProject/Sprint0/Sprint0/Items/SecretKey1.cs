69de8d5f4065a16c8bcccb1916b68ef0dea6334e
Phil Brocker <brocker.15@osu.edu>
2021-04-04 20:55:11 -0400

    Added new key item classes for keys that appear after defeating enemies.

---
+++ ZeldaProject/Sprint0/Sprint0/Items/SecretKey1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace Sprint0
+{
+    public class SecretKey1 : IItem
+    {
+        private Rectangle destination;
+        private Rectangle spriteSource;
+        private Texture2D sheet;
+        private List<INPC> npcs;
+
+
+        public SecretKey1(Rectangle startPos, Rectangle source, Texture2D spriteSheet, List<INPC> NPCS)
+        {
+            destination = startPos;
+            spriteSource = source;
+            sheet = spriteSheet;
+            npcs = NPCS;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if(npcs.Count == 0)
+            {
+                spriteBatch.Draw(sheet, destination, spriteSource, Color.White);
+            }            
+        }
+
+        public Rectangle GetLocationRectangle()
+        {
+            return destination;
+        }
+    }
+}
