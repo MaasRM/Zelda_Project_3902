79ca9e5bcc9167aea1612ca2e96c6d960726ac68
Riley Maas <rileymaas.rm@gmail.com>
2021-04-04 21:41:25 -0400

    Added InventoryCommand to Keyboard Controller

---
+++ ZeldaProject/Sprint0/Sprint0/Commands/InventoryCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Sprint0
+{
+    public class InventoryCommand : ICommand
+    {
+        private Sprint3 game;
+        public InventoryCommand(Sprint3 sprint)
+        {
+            game = sprint;
+        }
+
+        public void Execute()
+        {
+            
+        }
+    }
+}
