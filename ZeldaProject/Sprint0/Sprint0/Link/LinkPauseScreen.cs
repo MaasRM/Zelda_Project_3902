0a4a69e084feae5d9c1e6195a8ea531b4bfc817b
Riley <Riley@DESKTOP-9TP96O7>
2021-04-05 20:38:56 -0400

    The full pause screen now shows when the pause button is pressed

--- ZeldaProject/Sprint0/Sprint0/Link/LinkPauseScreen.cs
+++ ZeldaProject/Sprint0/Sprint0/Link/LinkPauseScreen.cs
@@ -13,12 +13,27 @@ namespace Sprint0
         private Boolean isPaused;
         private int currentYOffset;
         private static int incrementYSize = 10;
+        private Rectangle pauseTopSourceRectangle;
+        private Rectangle pauseBottomSourceRectangle;
 
         public LinkPauseScreen(Texture2D background)
         {
             isPaused = false;
             inventoryBackground = background;
             currentYOffset = 0;
+            pauseTopSourceRectangle = new Rectangle(1, 12, 255, 87);
+            pauseBottomSourceRectangle = new Rectangle(258, 113, 255, 87);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle blackSpaceSource = new Rectangle(280, 30, 1, 1);
+            Rectangle blackSpaceDestination = new Rectangle(0, -700 + currentYOffset, 256 * GameConstants.SCALE, 200 * GameConstants.SCALE);
+            spriteBatch.Draw(inventoryBackground, blackSpaceDestination, blackSpaceSource, Color.White);
+            Rectangle pauseTopDestination = new Rectangle(0, -700 + currentYOffset, 256 * GameConstants.SCALE, 64 * GameConstants.SCALE);
+            Rectangle pauseBottomDestination = new Rectangle(0, -352 + currentYOffset, 256 * GameConstants.SCALE, 64 * GameConstants.SCALE);
+            spriteBatch.Draw(inventoryBackground, pauseTopDestination, pauseTopSourceRectangle, Color.White);
+            spriteBatch.Draw(inventoryBackground, pauseBottomDestination, pauseBottomSourceRectangle, Color.White);
         }
 
         public void setGamePaused(Boolean val)
