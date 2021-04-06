d822563b6ec623fc4e3be7852660661800939e3f
Jared Zins <zins.14@buckeyemail.osu.edu>
2021-04-05 22:18:36 -0400

    Inventory right and left

---
+++ ZeldaProject/Sprint0/Sprint0/Commands/InventorySelectRightCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Sprint0
+{
+    public class InventorySelectRightCommand : ICommand
+    {
+        private Sprint4 game;
+        public InventorySelectRightCommand(Sprint4 sprint)
+        {
+            game = sprint;
+        }
+
+        public void Execute()
+        {
+            
+        }
+    }
+}
