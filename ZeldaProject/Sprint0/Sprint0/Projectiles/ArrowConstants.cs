48a0c859cd5e9810f0cbecf9da7d366a5a56d28d
nathanschultz <schultz.722@buckeyemail.osu.edu>
2021-04-11 15:41:52 -0400

    Adjusted Arrow Constants

--- ZeldaProject/Sprint0/Sprint0/Projectiles/ArrowConstants.cs
+++ ZeldaProject/Sprint0/Sprint0/Projectiles/ArrowConstants.cs
@@ -8,7 +8,9 @@ namespace Sprint0
         public const int BLUEDAMAGE = 2;
         public const int ArrowLength = 60;
         public const int ArrowWidth = 30;
-        public const int HITFRAME = 16;
-        public const int REMOVEFRAME = 19;
+        public const int BLUEHITFRAME = 16;
+        public const int BLUEREMOVEFRAME = 19;
+        public const int BROWNHITFRAME = 10;
+        public const int BROWNREMOVEFRAME = 13;
     }
 }
