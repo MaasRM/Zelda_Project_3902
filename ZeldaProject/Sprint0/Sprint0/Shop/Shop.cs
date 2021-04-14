2acb8fecc3a8c3599770b7dfffac2f46af380491
Jacob Kline <jrksoccer823@yahoo.com>
2021-04-14 15:32:33 -0400

    Initial shop push

---
+++ ZeldaProject/Sprint0/Sprint0/Shop/Shop.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0.Shop
+{
+    class Shop
+    {
+        INPC OldMan;
+        ShopText oldManText;
+        LinkInventory linkInv;
+        List<IItem> shopItems;
+        List<ItemText> itemPrices;
+        //sword upgrade, bombs, blue arrow/boomerang upgrade, armor/color upgrade
+
+        public Shop(LinkInventory inv, Texture2D npcSheet)
+        {
+            OldMan = new OldMan(100, 100, npcSheet);
+            oldManText = new ShopText();
+            linkInv = inv;
+            shopItems = new List<IItem>();
+            itemPrices = new List<ItemText>();
+        }
+
+    }
+}
