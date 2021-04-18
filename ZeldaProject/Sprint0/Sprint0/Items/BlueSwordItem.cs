5c77f4c493473e414a1026dde8cee116f2d10f7b
Jacob Kline <jrksoccer823@yahoo.com>
2021-04-18 19:07:27 -0400

    Added Sword item classes

---
+++ ZeldaProject/Sprint0/Sprint0/Items/BlueSwordItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace Sprint0
+{
+    public class BlueSwordItem : IItem
+    {
+        private Rectangle destination;
+        private Rectangle spriteSource;
+        private Texture2D sheet;
+
+
+        public BlueSwordItem(Rectangle startPos, Rectangle source, Texture2D spriteSheet)
+        {
+            destination = startPos;
+            spriteSource = source;
+            sheet = spriteSheet;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Draw(sheet, destination, spriteSource, Color.White);
+
+        }
+
+        public Rectangle GetLocationRectangle()
+        {
+            return destination;
+        }
+
+        public Rectangle GetSourceRectangle()
+        {
+            return spriteSource;
+        }
+
+        public Texture2D GetSpriteSheet()
+        {
+            return sheet;
+        }
+    }
+}
