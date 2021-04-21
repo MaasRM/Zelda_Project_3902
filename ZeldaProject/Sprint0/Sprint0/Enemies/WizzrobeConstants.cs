a4e0674bfea7e9a2110f7babc0b85d70fc3302be
nathanschultz <schultz.722@buckeyemail.osu.edu>
2021-04-21 15:01:12 -0400

    Base wizzrobe stuff

--- ZeldaProject/Sprint0/Sprint0/Enemies/WizzrobeConstants.cs
+++ ZeldaProject/Sprint0/Sprint0/Enemies/WizzrobeConstants.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 namespace Sprint0
 {
-    public class WizzrobeConstants
+    public static class WizzrobeConstants
     {
-        public WizzrobeConstants()
-        {
-        }
+        public const int WIDTHANDHEIGHT = 16;
+        public const int REDMAXHEALTH = 3;
+        public const int BLUEMAXHEALTH = 6;
+        public const int moveDist = 2;
+        public const int STUNFRAMECOUNT = 30;
+        public const int DAMAGEFRAMECOUNT = 8;
+        public const int FRAMESBEFORETELEPORT = 8;
+        public const int TELEPORTFRAMES = 10;
+        public const int REDDAMAGE = 1;
+        public const int BLUEDAMAGE = 2;
+        public const int FIRECHANCE = 64;
+        public const int CHANGEDIRECTION = 4;
     }
 }
