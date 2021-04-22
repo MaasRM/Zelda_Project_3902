249487949b6f2069e576d112a1445896713bc86f
nathanschultz <schultz.722@buckeyemail.osu.edu>
2021-04-22 00:03:42 -0400

    Adjust Gohma Constants

--- ZeldaProject/Sprint0/Sprint0/Enemies/GohmaConstants.cs
+++ ZeldaProject/Sprint0/Sprint0/Enemies/GohmaConstants.cs
@@ -6,12 +6,12 @@ namespace Sprint0
         public const int WIDTH = 48;
         public const int HEIGHT = 16;
         public const int moveDist = 2;
-        public const int FIRECHANCE = 30;
-        public const int FIRECOOLDOWN = 10;
+        public const int FIRECHANCE = 20;
+        public const int FIRECOOLDOWN = 15;
         public const int CHANGEDIRECTIONFRAME = 8;
         public const int EYETRANSITIONFRAMES = 2;
-        public const int EYEOPENFRAMES = 10;
-        public const int CLOSEFRAMEMAX = 20;
-        public static Direction[] Directions = { Direction.Right, Direction.Down, Direction.Up, Direction.Right, Direction.Left, Direction.Down, Direction.Up, Direction.Left };
+        public const int EYEOPENFRAMES = 15;
+        public const int CLOSEFRAMEMAX = 30;
+        public static Direction[] Directions = { Direction.Right, Direction.Down, Direction.Up, Direction.Right, Direction.Right, Direction.Left, Direction.Left, Direction.Down, Direction.Up, Direction.Left, Direction.Left, Direction.Right };
     }
 }
